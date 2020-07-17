bb7096521cb0ecc963d718a995368f8dadd3be7e
Josh Rainwater <jrainwater@intellaegis.com>
2020-07-17 14:27:53 -0700

    Simpler again.

--- go.js
+++ go.js
@@ -6,9 +6,7 @@ if(!process.argv[2]) {
 }
 
 let bgData = require(`./${process.argv[2]}`);
-let mdString = makeBGMarkdownTable(bgData, 'output.md');
-
-fs.writeFileSync('output.md', mdString);
+fs.writeFileSync('output.md', makeBGMarkdownTable(bgData, 'output.md'));
 
 console.log('Done');
 process.exit();
@@ -23,44 +21,33 @@ function makeBGMarkdownTable(bgData, fileName) {
     bgData.locations.map((loc) =>   { locationsById[loc.id] = loc.name; });
     
     let prettyFile = [
-        '# Board Game Tick List',
-        '',
+        '# Board Game Tick List', '',
         'Date | Game | Location | Players | Winner |',
         '--- | --- | --- | --- | --- |'
     ];
     let plays = [];
     bgData.plays.forEach((play) => {
 
-        let row = [
-            play.playDate.substring(0,10), //date,
-            bgData.games.filter(g => g.id == play.gameRefId)[0].name, // game
-            (play.locationRefId) ? locationsById[play.locationRefId] : '' // location
-            // players & scores
-            // winner(s)
-        ];
-        if(play.board) { row[1] += ` [${play.board}]`}
-
-        let players = [];
-        let winners = [];
+        let players = [], winners = [];
         play.playerScores.forEach(score => {
             let name = bgData.players.filter(g => g.id == score.playerRefId)[0].name
             if(score.role) {
-                name += ` [${score.role}]`;
+                name += ` (${score.role})`;
             }
-            if(score.score) {
-                players.push(`${name} ${score.score}`);
-            } else {
-                players.push(`${name}`);
+            players.push([name, score.score].join(' '));
+            if(score.winner) {
+                winners.push(name);
             }
         });
+        if(!winners.length) { winners.push('Lost'); }
 
-        if(!winners.length) {
-            winners.push('Lost');
-        }
-        row.push(players.join(', '));
-        row.push(winners.join(', '));
-
-        plays.push(row.join(' | '));
+        plays.push([
+            play.playDate.substring(0,10), //date,
+            (play.board) ? bgData.games.filter(g => g.id == play.gameRefId)[0].name + ` [${play.board}]` : bgData.games.filter(g => g.id == play.gameRefId)[0].name, // game
+            (play.locationRefId) ? locationsById[play.locationRefId] : '', // location
+            players.join(', '), // players
+            winners.join(', ') // winner(s)
+        ].join(' | '));
     });
 
     return prettyFile.concat(plays.reverse()).join('\n');
